614c2ff5e0637db6fa87320ea9929dd38f584eaa
Rafat Rahman Tushar <rafat.tushar@northsouth.edu>
2019-07-08 01:31:51 +0600

    Solving Issue #13 Create a Log In Screen

---
+++ Project Code/app/src/main/java/edu/rafat/tushar/onlinestore/LogInScreen.java
@@ -0,0 +1,25 @@
+package edu.rafat.tushar.onlinestore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LogInScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in_screen);
+
+        Button logInBtn = (Button) findViewById(R.id.logInBtn);
+
+        logInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Activity
+            }
+        });
+    }
+}
