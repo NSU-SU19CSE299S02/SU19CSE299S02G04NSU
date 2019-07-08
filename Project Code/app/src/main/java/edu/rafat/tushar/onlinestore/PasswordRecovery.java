5773e7cdf956d464a5ef12c9a32078e15f83f51f
SHAN2288 <nuruzzaman@northsouth.edu>
2019-07-08 23:20:00 +0600

    Solving Issue#14 Password Recovery

---
+++ Project Code/app/src/main/java/edu/rafat/tushar/onlinestore/PasswordRecovery.java
@@ -0,0 +1,14 @@
+package edu.rafat.tushar.onlinestore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PasswordRecovery extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password_recovery);
+    }
+}
