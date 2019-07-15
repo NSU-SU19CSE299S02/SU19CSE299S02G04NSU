e305b23b609b4a93aa5c316b202a74c9e1a883e6
SHAN2288 <nuruzzaman@northsouth.edu>
2019-07-16 00:55:45 +0600

    Feature: Solving issue #20 Store Info
    1. Store information in Screen

---
+++ Project Code/app/src/main/java/edu/rafat/tushar/onlinestore/StoreInfo.java
@@ -0,0 +1,14 @@
+package edu.rafat.tushar.onlinestore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StoreInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_store_info);
+    }
+}
