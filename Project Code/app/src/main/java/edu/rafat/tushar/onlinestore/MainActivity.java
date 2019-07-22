8d637db08cf6ceb16fd4c4dd55b7c9a580598fdf
SHAN2288 <nuruzzaman@northsouth.edu>
2019-07-22 23:28:57 +0600

    Feature: #24 Profile screen integration
    Adding the screen, what will be shown after the click on the profile Info button.

--- Project Code/app/src/main/java/edu/rafat/tushar/onlinestore/MainActivity.java
+++ Project Code/app/src/main/java/edu/rafat/tushar/onlinestore/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
 
@@ -26,5 +27,9 @@ public class MainActivity extends AppCompatActivity {
 
     public void btn_storeInfo(View view) {
         startActivity(new Intent(getApplicationContext(),StoreInfo.class));
+    }
+
+    public void btn_profile(View view) {
+        startActivity(new Intent(getApplicationContext(),ProfileScreen.class));
     }
 }
