877f5f4d6856f4a2e7ee691d686be1e8942feec6
SHAN2288 <nuruzzaman@northsouth.edu>
2019-07-29 23:04:37 +0600

    Feature #16 : Updating Home Screen
    1. Adding naavigation bar
    2, Updating design

--- Project Code/app/src/main/java/edu/rafat/tushar/onlinestore/MainActivity.java
+++ Project Code/app/src/main/java/edu/rafat/tushar/onlinestore/MainActivity.java
@@ -15,14 +15,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button homeBtn = (Button) findViewById(R.id.homeBtn);
-
-        homeBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //Text,activity
-            }
-        });
     }
 
     public void btn_storeInfo(View view) {
