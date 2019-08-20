77afb70021c60f25f7cdefbe8c4c8b37f9460547
SHAN2288 <nuruzzaman@northsouth.edu>
2019-08-21 00:15:16 +0600

    Feature #41 Sign Up Integration
    1. Adding the library

--- Project Code/app/src/main/java/edu/rafat/tushar/onlinestore/SignUp.java
+++ Project Code/app/src/main/java/edu/rafat/tushar/onlinestore/SignUp.java
@@ -1,30 +1,46 @@
 package edu.rafat.tushar.onlinestore;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignUp extends AppCompatActivity implements View.OnClickListener{
 
     private EditText FullNameEditText, UserNameEditText, EmailEditText,
             ConfirmEmailEditText, PasswordEditText,ConfirmPasswordEditText;
     private Button Register_button;
+    private FirebaseAuth mAuth;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        this.setTitle("Sign Up");
 
-        FullNameEditText = findViewById(R.id.FullNameEditText);
-        UserNameEditText = findViewById(R.id.UserNameEditText);
+        mAuth = FirebaseAuth.getInstance();
+        progressBar = (ProgressBar) findViewById(R.id.progressbarId);
+
+       // FullNameEditText = findViewById(R.id.FullNameEditText);
+        //UserNameEditText = findViewById(R.id.UserNameEditText);
         EmailEditText = findViewById(R.id.EmailEditText);
-        ConfirmEmailEditText = findViewById(R.id.ConfirmEmailEditText);
+        //ConfirmEmailEditText = findViewById(R.id.ConfirmEmailEditText);
         PasswordEditText = findViewById(R.id.PasswordEditText);
-        ConfirmPasswordEditText = findViewById(R.id.ConfirmPasswordEditText);
+        //ConfirmPasswordEditText = findViewById(R.id.ConfirmPasswordEditText);
 
         Register_button.setOnClickListener(this);
 
@@ -47,7 +63,7 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener{
        // String FName = FullNameEditText.getText().toString().trim();
       //  String UName = UserNameEditText.getText().toString().trim();
         String Email = EmailEditText.getText().toString().trim();
-        String CEmail = ConfirmEmailEditText.getText().toString().trim();
+        //String CEmail = ConfirmEmailEditText.getText().toString().trim();
         String Pass = PasswordEditText.getText().toString().trim();
       //  String CPass = ConfirmPasswordEditText.getText().toString().trim();
 
@@ -66,20 +82,6 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener{
             return;
         }
 
-        if(CEmail.isEmpty())
-        {
-            ConfirmPasswordEditText.setError("Enter an email address");
-            ConfirmPasswordEditText.requestFocus();
-            return;
-        }
-
-        if(!android.util.Patterns.EMAIL_ADDRESS.matcher(Email).matches())
-        {
-            ConfirmPasswordEditText.setError("Enter a valid email address");
-            ConfirmPasswordEditText.requestFocus();
-            return;
-        }
-
         if(Pass.isEmpty())
         {
             PasswordEditText.setError("Enter a password");
@@ -93,9 +95,22 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener{
             PasswordEditText.requestFocus();
             return;
         }
+        progressBar.setVisibility(View.VISIBLE);
 
+        mAuth.createUserWithEmailAndPassword(Email,Pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                progressBar.setVisibility(View.GONE);
+                if (task.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(), "Register is successful", Toast.LENGTH_SHORT).show();
 
+                } else {
+                    Toast.makeText(getApplicationContext(), "Register is not successful", Toast.LENGTH_SHORT).show();
 
+                }
+
+            }
+        })
 
 
 
