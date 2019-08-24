29b64aab0daa4ebf1ec6b5ab166914977b5245c2
SHAN2288 <nuruzzaman@northsouth.edu>
2019-08-25 01:38:16 +0600

    Feature #40 Online_payment_system
    1. Adding the payment form

---
+++ Project Code/app/src/main/java/edu/rafat/tushar/onlinestore/Payment_form.java
@@ -0,0 +1,38 @@
+package edu.rafat.tushar.onlinestore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.craftman.cardform.Card;
+import com.craftman.cardform.CardForm;
+import com.craftman.cardform.OnPayBtnClickListner;
+import com.google.android.gms.common.internal.Objects;
+
+public class Payment_form extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_form);
+
+        CardForm cardForm = (CardForm)findViewById(R.id.cardform);
+        TextView textDes = (TextView)findViewById(R.id.payment_amount);
+        Button btnPay = (Button)findViewById(R.id.btn_pay);
+
+        textDes.setText("$1999");
+        btnPay.setText(String.format("payer %s",textDes.getText()));
+
+        cardForm.setPayBtnClickListner(new OnPayBtnClickListner() {
+            @Override
+            public void onClick(Card card) {
+
+                Toast.makeText(Payment_form.this, "Name:", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+     }
+}
